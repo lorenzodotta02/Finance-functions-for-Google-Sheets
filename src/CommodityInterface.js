c06f0c8fc44e2f38ebf0d2f2de3c12a41288a50b
Lorenzo Dotta <74047289+lorenzodotta02@users.noreply.github.com>
2025-11-29 12:39:16 +0100

    Update CommodityInterface.js

--- src/CommodityInterface.js
+++ src/CommodityInterface.js
@@ -1,15 +1,9 @@
 /**
  * Returns the current spot price of a commodity.
- *
- * Currently supports:
- *   - Gold → price in EUR per gram
- *   - Silver → price in EUR per gram
- *   - Platinum → price in EUR per gram
- *   - Palladium → price in EUR per gram
- *
- * @param {string} date The reference date cell (Utils!$A$1).
- *                      This can be used to trigger updates in the sheet.
- * @param {string} name The name of the commodity.
+ * 
+ * @param {Utils!$A$1} date The reference date cell (Utils!$A$1)
+ *                          This is required to ensure that price quotes stay updated
+ * @param {"Gold"} name The name of the commodity.
  *                      Supported values: "Gold", "Silver", "Platinum", "Palladium".
  * @returns {number} The current spot price of the specified commodity in EUR per gram.
  * @customfunction
