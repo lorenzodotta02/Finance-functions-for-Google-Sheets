b8c1577115fc25b3e3816adc4f54258e235c8bd6
Lorenzo Dotta <74047289+lorenzodotta02@users.noreply.github.com>
2025-11-29 12:38:25 +0100

    Update BondInterface.js

--- src/BondInterface.js
+++ src/BondInterface.js
@@ -1,12 +1,10 @@
 /**
  * Returns the current market price of a bond.
+ * Currently supports: Bonds traded on Borsa Italiana.
  *
- * Currently supports:
- *   - Bonds traded on Borsa Italiana.
- *
- * @param {string} date The reference date cell (Utils!$A$1).
- *                      This can be used to trigger updates in the sheet.
- * @param {string} isin The ISIN code of the bond.
+ * @param {Utils!$A$1} date The reference date cell (Utils!$A$1)
+ *                          This is required to ensure that price quotes stay updated
+ * @param {"IT0005672024"} isin The ISIN code of the bond.
  * @returns {number} The current market price of the specified bond.
  * @customfunction
  */
