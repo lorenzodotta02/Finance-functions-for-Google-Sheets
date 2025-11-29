fdd2958473f487f0d126dc1b80e3ebbc85536c17
Lorenzo Dotta <74047289+lorenzodotta02@users.noreply.github.com>
2025-11-29 12:40:24 +0100

    Update Crypto.js

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,34 +1,40 @@
 function cryptoPrice(symbol) {
-  var url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
-  var apiKey = PropertiesService.getScriptProperties().getProperty('CMC_API_KEY'); 
-  
+
+  var url = URL_CMC_QUOTES;
+  var apiKey = PropertiesService.getScriptProperties().getProperty(PROP_CMC_API_KEY);
   if (!apiKey) {
-    throw new Error("Missing API key for CoinMarketCap. Add it to Script Properties:\n" +
-                    "property = CMC_API_KEY\n" +
-                    "value    = Your API Key");
+    throw new Error(
+      "Missing API key for CoinMarketCap. Add it to Script Properties:\n" +
+      "property = " + PROP_CMC_API_KEY + "\nvalue    = Your API Key"
+    );
   }
 
   var headers = {
-    "X-CMC_PRO_API_KEY": apiKey,
-    "Accept": "application/json"
+    [HEADER_CMC_APIKEY]: apiKey,
+    "Accept": HEADER_CMC_ACCEPT
   };
 
   var parameters = {
     "symbol": symbol,
-    "convert": "EUR"
+    "convert": CMC_CONVERT_TO
   };
 
   var queryString = Object.keys(parameters)
-    .map(function(key) { return key + '=' + encodeURIComponent(parameters[key]); })
+    .map(function (key) {
+      return key + '=' + encodeURIComponent(parameters[key]);
+    })
     .join('&');
 
   try {
-    var response = UrlFetchApp.fetch(url + "?" + queryString, { 'headers': headers, muteHttpExceptions: true });
-    
+    var response = UrlFetchApp.fetch(url + "?" + queryString, {
+      headers: headers,
+      muteHttpExceptions: true
+    });
+
     if (response.getResponseCode() !== 200) {
       throw new Error("HTTP error: " + response.getResponseCode());
     }
-    
+
     var json;
     try {
       json = JSON.parse(response.getContentText());
@@ -36,16 +42,21 @@ function cryptoPrice(symbol) {
       throw new Error("Invalid JSON response: " + parseErr.message);
     }
 
-    if (!json.data || !json.data[symbol] || !json.data[symbol].quote || !json.data[symbol].quote.EUR) {
+    if (!json.data ||
+      !json.data[symbol] ||
+      !json.data[symbol].quote ||
+      !json.data[symbol].quote[CMC_CONVERT_TO]) {
       throw new Error("Crypto not found or invalid API response: " + response.getContentText());
     }
 
-    var price = json.data[symbol].quote.EUR.price;
+    var price = json.data[symbol].quote[CMC_CONVERT_TO].price;
+
     if (typeof price !== "number" || isNaN(price) || price <= 0) {
       throw new Error("Invalid price received: " + price);
     }
 
     savePrice(symbol, price);
+
     return price;
 
   } catch (err) {
