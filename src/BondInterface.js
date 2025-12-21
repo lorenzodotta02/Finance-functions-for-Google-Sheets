3e88f400d51df64ad80fcdbb6d88346847a4eaa9
Lorenzo Dotta <74047289+lorenzodotta02@users.noreply.github.com>
2025-12-21 14:58:26 +0100

    Update BondInterface.js

--- src/BondInterface.js
+++ src/BondInterface.js
@@ -5,9 +5,10 @@
  * @param {Utils!$A$1} date The reference date cell (Utils!$A$1)
  *                          This is required to ensure that price quotes stay updated
  * @param {"IT0005672024"} isin The ISIN code of the bond.
+ *  @param {"MOTX"} stockExchange The stock exchange MIC (ISO 10383)
  * @returns {number} The current market price of the specified bond.
  * @customfunction
  */
-function BONDPRICE(date, isin) {
-  return bondPrice(isin);
+function BONDPRICE(date, isin, stockExchange) {
+  return bondPrice(isin, stockExchange);
 }
